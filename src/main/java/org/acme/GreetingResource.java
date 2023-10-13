b3294a0f727e1fe0fc2f6d8eb52a27644f284501
Riccardo Pasquini <r.pasquini@almawave.it>
2023-10-13 18:53:09 +0200

    CDI misusage
    https://stackoverflow.com/questions/72142018/cdi-bean-lack-of-value-when-accessed-through-public-field
    
    Signed-off-by: Riccardo Pasquini <r.pasquini@almawave.it>

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import io.quarkus.logging.Log;
 import org.apache.zookeeper.KeeperException;
@@ -19,7 +20,7 @@ public class GreetingResource {
     public String hello() {
 
         try {
-            zookeeperService.client.getData("/test", false, zookeeperService.client.exists("/test", false));
+            zookeeperService.getClient().getData("/test", false, zookeeperService.getClient().exists("/test", false));
         } catch (KeeperException | InterruptedException e) {
             Log.error(e.getMessage());
         }
--- src/main/java/org/acme/ZookeeperService.java
+++ src/main/java/org/acme/ZookeeperService.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import io.quarkus.arc.AsyncObserverExceptionHandler;
 import io.quarkus.logging.Log;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.ObservesAsync;
 import jakarta.enterprise.inject.spi.EventContext;
@@ -16,11 +17,21 @@ public class ZookeeperService implements AsyncObserverExceptionHandler {
     @Inject
     ZooKeeper client;
 
+    @PostConstruct
+    void init() {
+        assert client != null;
+        Log.debug("client granted");
+    }
+
     public void onZKEvent(@ObservesAsync WatchedEvent event) {
         Log.infof("Receiving [%s]", event.getState());
     }
-
+    
     @Override
     public void handle(Throwable throwable, ObserverMethod<?> observerMethod, EventContext<?> eventContext) {
+    }
+
+    public ZooKeeper getClient() {
+        return client;
     }
 }
